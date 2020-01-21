5c34cf7b34b76bacde8973067cbc5fbd99b95ae3
LeoooY <leoY.dev@gmail.com>
2020-01-21 18:35:44 +0800

    add modal

--- pages/index/subcomponents/main/subcomponents/toolbar/index.jsx
+++ pages/index/subcomponents/main/subcomponents/toolbar/index.jsx
@@ -1,6 +1,8 @@
-import css from './index.less';
-import { Button, Input } from 'antd';
+import { useState, useCallback,useReducer } from 'react';
+import {cloneDeep } from 'lodash';
+import { Button, Input, Modal } from 'antd';
 const { Search } = Input;
+import css from './index.less';
 
 const conf = [
     {
@@ -15,23 +17,57 @@ const conf = [
     }
 ]
 export default () => {
+    const [modal,dispatchModal]=useReducer((state,action)=>{
+        const newState=cloneDeep(state);
+        switch(action.type){
+            case 'show':
+                newState.visible=true
+                newState.title=action.data.title;
+                return newState
+            case 'hide':
+                newState.visible=false
+                return newState
+            default:
+                return state
+        }
+    },{
+        visible:false,
+        title:'新增'
+    });
+    
+    const cancleModal=useCallback(()=>{
+        dispatchModal({type:'hide'})
+    },[dispatchModal])
+
+    const showModal=useCallback((e,opt)=>{
+        const {text}=opt
+        dispatchModal({type:'show',data:{title:text}})
+    },[dispatchModal])
+
+
     return (
-        <>
         <div className={css.ToolBar}>
             {conf.map((el, idx) => {
                 const { text, type, shape, marginRight } = el;
                 return <Button key={idx}
                     type={type}
                     style={{ marginRight: marginRight || 10 }}
+                    onClick={e=>showModal(e,{text})}
                     shape={shape}>
                     {text}
                 </Button>
             })}
-            <Search 
+            <Search
                 className={css.Search}
-                enterButton/>    
+                enterButton />
+            
+            <Modal  
+                visible={modal.visible}
+                title={modal.title}
+                getContainer={false}
+                onCancel={cancleModal}>
+                {modal.title}
+            </Modal>
         </div>
-        
-        </>
     )
 }
---
+++ pages/index/subcomponents/main/subcomponents/uploadModal/index.jsx
@@ -0,0 +1,8 @@
+// import { Modal } from 'antd';
+
+// export default (props) => {
+//     const { children } = props;
+//     return (
+//         children}
+//     )
+// }
