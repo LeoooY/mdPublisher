c8563708ff6443c273a2ec0295cc4bc96e1c5f02
LeoooY <li.yang@jiediankeji.com>
2020-04-08 10:27:05 +0800

    feat: sqlite api 使用v4版本

--- server/sqlite3/db.js
+++ server/sqlite3/db.js
@@ -1,60 +1,36 @@
 const sqlite3 = require('sqlite3').verbose();
+const { open } = require('sqlite')
+/* 
+    Api
+    https://www.npmjs.com/package/sqlite 
+*/
+
 const path = require('path')
 const dbpath = path.resolve('server/sqlite3/mdfiles.db')
-// console.log('dbpath',dbpath)
+/* console.log('dbpath',dbpath) */
 
 const connDB = async () => {
-    let db;
-    const rs = await new Promise((resolve, reject) => {
-        db = new sqlite3.Database(dbpath, (err) => {
-            if (err) {
-                console.error('SQLite Open Error', err.message);
-                reject(-1)
-            }
-            console.log('SQLite Connected!');
-            resolve(1)
-        });
-    }).catch(rs=>rs)
-    return rs>0 ? db : undefined
+    try {
+        const db = await open({
+            filename: dbpath,
+            driver: sqlite3.Database
+        })        
+        return db;
+    } catch (err) {
+        return;
+    }
 }
-
-/* db.serialize(() => {
-    db.each(`SELECT *
-             FROM mdLists`, (err, row) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('row', row.date, row.uuid, row.name);
-
-    });
-}); */
-
-const insertDB = (db) => {
-
-}
-
-const updateDB = (db) => {
-
-}
-
-const findDB = (db) => {
-
-}
-
 
 const closeDB = (db) => {
-    db.close((err) => {
-        if (err) {
-            console.error('SQLite Close Error', err.message);
-        }
-        console.log('SQLite Closed!');
-    });
+    try {
+        await db.close()
+        return 1;
+    } catch (err) {
+        return;
+    }
 }
 
 module.exports = {
     connDB,
     closeDB,
-    findDB,
-    insertDB,
-    updateDB,
 }
